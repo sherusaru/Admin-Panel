6231ec13b5c21b61c23db4c4b4f26a9f63c1c89b
sherusaru10 <sherusaru07@gmail.com>
2023-03-03 19:13:47 +0545

    Chart Widget

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 5000,
+  },
+  {
+    name: "March",
+    "Active User": 3000,
+  },
+  {
+    name: "Apr",
+    "Active User": 2000,
+  },
+  {
+    name: "May",
+    "Active User": 2500,
+  },
+  {
+    name: "Jun",
+    "Active User": 2800,
+  },
+  {
+    name: "Jul",
+    "Active User": 2200,
+  },
+  {
+    name: "Aug",
+    "Active User": 2400,
+  },
+  {
+    name: "Sep",
+    "Active User": 3200,
+  },
+  {
+    name: "Oct",
+    "Active User": 2780,
+  },
+  {
+    name: "Nov",
+    "Active User": 4200,
+  },
+  {
+    name: "Dec",
+    "Active User": 4870,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../dummyData";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 }
